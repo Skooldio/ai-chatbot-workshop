dede838a977d144c4f4aeab0b654ac8e7d909f73
inatnun <natthanun@skooldio.tech>
2024-09-13 00:38:46 +0700

    Add gemini

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 import os
 import time
 import streamlit as st
+import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyCGjhOruoVxcmKe_OdAgolfHJRhTupzoG4")
+
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-pro-exp-0827",
+    generation_config=generation_config,
+    # safety_settings = Adjust safety settings
+    # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+chat_session = model.start_chat()
 
 st.title("💬 Chatbot")
 
@@ -15,8 +34,10 @@ for msg in st.session_state.messages:
 
 
 def get_chat_response(prompt):
-    return prompt
-    
+    response = chat_session.send_message(prompt)
+    return response.text
+
+
 def stream_response(response):
     for chunk in response:
         for ch in chunk.text.split(" "):
@@ -34,4 +55,3 @@ if prompt := st.chat_input():
         response = get_chat_response(prompt)
         st.write(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
-

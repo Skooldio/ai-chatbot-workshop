7bf2ea343e60e91e797261b2f9ef47b76e904187
inatnun <natthanun@skooldio.tech>
2024-09-13 00:19:22 +0700

    Fix code to prepare template

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import time
 import streamlit as st
 
 
@@ -14,12 +16,21 @@ for msg in st.session_state.messages:
 
 def get_chat_response(prompt):
     return prompt
+    
+def stream_response(response):
+    for chunk in response:
+        for ch in chunk.text.split(" "):
+            yield ch
+            time.sleep(0.05)
 
 
 if prompt := st.chat_input():
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    msg = get_chat_response(prompt)
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+    with st.chat_message(
+        "assistant",
+    ):
+        response = get_chat_response(prompt)
+        msg = st.write(response)
+        st.session_state.messages.append({"role": "assistant", "content": msg})
